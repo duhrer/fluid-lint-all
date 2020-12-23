9a83fc3b3af5e123f1a05fcb68fbb43a157df97c
Tony Atkins <tony@raisingthefloor.org>
2020-12-23 10:36:20 +0100

    GH-1: Prevented ESLint from crashing overall run if no configuration file is found.

--- src/js/eslint.js
+++ src/js/eslint.js
@@ -22,29 +22,34 @@ fluid.lintAll.eslint = function (options) {
     var filesToScan = fluid.glob.findFiles(options.rootPath, options.includes, options.excludes, options.minimatchOptions);
 
     if (filesToScan.length) {
-        var validationResults = engine.executeOnFiles(filesToScan);
-        fluid.each(validationResults.results, function (singleFileResults) {
-            toReturn.checked++;
-            if (singleFileResults.errorCount) {
-                toReturn.invalid++;
-                var formattedErrors = [];
-                fluid.each(singleFileResults.messages, function (singleMessage) {
-                    var formattedMessage = singleMessage.message + " (" + singleMessage.ruleId + ")";
-                    formattedErrors.push({
-                        line: singleMessage.line,
-                        column: singleMessage.column,
-                        message: formattedMessage
+        try {
+            var validationResults = engine.executeOnFiles(filesToScan);
+            fluid.each(validationResults.results, function (singleFileResults) {
+                toReturn.checked++;
+                if (singleFileResults.errorCount) {
+                    toReturn.invalid++;
+                    var formattedErrors = [];
+                    fluid.each(singleFileResults.messages, function (singleMessage) {
+                        var formattedMessage = singleMessage.message + " (" + singleMessage.ruleId + ")";
+                        formattedErrors.push({
+                            line: singleMessage.line,
+                            column: singleMessage.column,
+                            message: formattedMessage
+                        });
                     });
-                });
-                if (formattedErrors.length) {
-                    var relativePath = path.relative(options.rootPath, singleFileResults.filePath);
-                    toReturn.errorsByPath[relativePath] = formattedErrors;
+                    if (formattedErrors.length) {
+                        var relativePath = path.relative(options.rootPath, singleFileResults.filePath);
+                        toReturn.errorsByPath[relativePath] = formattedErrors;
+                    }
                 }
-            }
-            else {
-                toReturn.valid++;
-            }
-        });
+                else {
+                    toReturn.valid++;
+                }
+            });
+        }
+        catch (error) {
+            fluid.log(fluid.logLevel.WARN, "Unable to run ESLint check(s): " + error.message);
+        }
     }
 
     return toReturn;
