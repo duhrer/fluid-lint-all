cff0a1132eac1f6b457ca8c9bd0f0d9db54b475e
Tony Atkins <tony@raisingthefloor.org>
2021-02-01 17:36:15 +0100

    GH-3: Add ability to exclude all files in `.gitignore` from linting.

--- tests/js/launcher-tests.js
+++ tests/js/launcher-tests.js
@@ -129,7 +129,18 @@ fluid.defaults("fluid.tests.lintAll.launcher.runner", {
             configFile: ".fluidlintallrc-nofiles.json",
             shouldBeInvalid: true,
             expectedMessage: ["ERROR: No files checked, please review your configuration and command line arguments."]
+        },
+        useGitIgnore: {
+            message: "We should be able to exclude files using a .gitignore file.",
+            configFile: ".fluidlintallrc-gitignore.json"
+        },
+        disableGitIgnore: {
+            message: "We should be able to exclude files using a .gitignore file.",
+            configFile: ".fluidlintallrc-gitignore-disabled.json",
+            shouldBeInvalid: true,
+            expectedMessage: "FAIL - One or more linting checks have errors."
         }
+
     },
     listeners: {
         "onCreate.runTests": {
