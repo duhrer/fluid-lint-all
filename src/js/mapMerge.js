a447469d2890dedb36eb49765bd908b01f840e51
Tony Atkins <tony@raisingthefloor.org>
2021-04-19 10:59:30 +0200

    GH-29: Added (JS)docs for new includes/excludes merging.

--- src/js/mapMerge.js
+++ src/js/mapMerge.js
@@ -2,11 +2,30 @@
 var fluid = require("infusion");
 fluid.registerNamespace("fluid.lintAll");
 
+/**
+ *
+ * A function to merge two arrays such that:
+ *
+ * - All entries in `toMergeArray` that do not already exist are added.
+ * - All entries in `toMergeArray` that already exist in `baseArray` are overwritten.
+ * - All entries in `baseArray` that are not referenced in `toMergeArray` are preserved.
+ *
+ * The strategy used here is to construct a map based on observed patterns (`true`) and negated patterns (`false`).
+ * This allows adding material to the defaults, and also provides a means of "negating" default material if needed.
+ *
+ * @param {Array} baseArray - An array of underlying values.
+ * @param {Array} toMergeArray - An array of values to "overlay" on `baseArray`.
+ * @return {Array} - The contents of both arrays, merged.
+ *
+ */
 fluid.lintAll.mapMerge = function (baseArray, toMergeArray) {
     var mergedObject = {};
     fluid.each([baseArray, toMergeArray], function (arrayToMerge) {
         fluid.each(arrayToMerge, function (singleValue) {
+            // Convert the glob pattern to a single-entry object ("leaf").
             var singleEntry = fluid.lintAll.globToMapEntry(singleValue);
+
+            // Merge the "leaf" with the existing results ("tree").
             fluid.extend(mergedObject, singleEntry);
         });
     });
@@ -15,6 +34,14 @@ fluid.lintAll.mapMerge = function (baseArray, toMergeArray) {
     return mergedArray;
 };
 
+/**
+ *
+ * Convert a merged map of `true` / `false` values into an array of positive and negated "glob" patterns.
+ *
+ * @param {Object} mergedMap - The map to convert.
+ * @return {Array<String>} - An array of "glob" patterns.
+ *
+ */
 fluid.lintAll.mergedMapToGlobs = function (mergedMap) {
     var globs = [];
     fluid.each(mergedMap, function (value, key) {
@@ -24,6 +51,15 @@ fluid.lintAll.mergedMapToGlobs = function (mergedMap) {
     return globs;
 };
 
+/**
+ *
+ * Convert a glob pattern to a single-entry object that can be merged with other converted glob patterns.
+ *
+ * @param {String} glob - A single "glob" pattern.
+ * @return {Object} - A single-entry object whose key is the glob pattern, and whose value indicates whether the
+ * value is positive or negated.
+ *
+ */
 fluid.lintAll.globToMapEntry = function (glob) {
     var isPositive = glob.indexOf("!") !== 0;
     var entryValue = isPositive ? glob : glob.substring(1);
